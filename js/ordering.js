f317f2cd1d4dd942bffd84cdc753e1fcbf32ee30
Camon-fi07 <79801380+Camon-fi07@users.noreply.github.com>
2023-02-03 19:24:07 +0700

    Add files via upload
    Переместил шрифты и изображения в assets, небольшое улучшение в получении всех и купленных продуктов, наполовину готовая форма

--- js/ordering.js
+++ js/ordering.js
@@ -6,26 +6,16 @@ const oldText = costOfOrdering.innerHTML;
 const orders = document.querySelector("#orders");
 orders.textContent = `Оформить заказ(${localStorage.getItem("count")})`;
 
-//Обрабокта всех пицц
-let allProducts = [];
-const countOfProducts = Number(localStorage.getItem("countOfProducts"));
-for (let index = 0; index < countOfProducts; index++) {
-    allProducts.push(JSON.parse(localStorage.getItem(index)));
-}
+// Получение всех продуктов
+const allProducts = JSON.parse(localStorage.getItem("allProducts"));
 
-// Обработка купленных пицц
-let boughtProducts = localStorage['boughtProducts'].split(",");
-for (const i in boughtProducts) {
-    boughtProducts[i] = {
-        count: 1,
-        index:boughtProducts[i],
-    }
-}
+// Получение купленных продуктов
+const boughtProducts = JSON.parse(localStorage.getItem("boughtProducts"));
 
 //Вычисление стоимости заказа
 let cost = 0;
 for (const element of boughtProducts) {
-    if (element.index != "") cost += allProducts[Number(element.index)].cost;
+    if (!!element.index) cost += allProducts[Number(element.index)].cost;
 }
 costOfOrdering.textContent = `${oldText} ${cost} ₽`
 
@@ -54,7 +44,7 @@ function addElement(index){
 }
 
 for (const element of boughtProducts) {
-    if (element.index != "") addElement(Number(element.index));
+    if (!!element.index) addElement(Number(element.index));
 }
 
 
@@ -63,12 +53,7 @@ function deleteProduct(product){
     for (const i in boughtProducts) {
         if(boughtProducts[i].index == product) boughtProducts.splice(i, 1);
     }
-
-    let newBoughtProducts = [];
-    for (const element of boughtProducts) {
-        newBoughtProducts.push(element.index);
-    }
-    localStorage.setItem("boughtProducts", newBoughtProducts.join(","));
+    localStorage.setItem("boughtProducts", JSON.stringify(boughtProducts));
 
     localStorage.setItem("count", localStorage.getItem("count") - 1);
     orders.textContent = `Оформить заказ(${localStorage.getItem("count")})`;
@@ -119,4 +104,94 @@ blockOfBoughtProducts.addEventListener("click", function(event){
         event.target.closest(".product__cancel").parentElement.remove();
         deleteProduct(indexOfProuct);
     }
+})
+
+const buttonSubmit = document.querySelector(".content__form-submit");
+
+
+// Форма
+
+function createNote(text){
+    let note = document.createElement("div");
+    note.classList.add("note");
+    note.textContent = text;
+    buttonSubmit.before(note);
+}
+
+// проверка длины тех или инх значений
+function check(order){
+    isRight = true;
+    if(order.details.user.firstname < 2){
+        createNote("длина имени должна быть не менее двух букв")
+        isRight = false;
+    }
+    if (order.details.user.lastname < 2){
+        createNote("длина фамилии должна быть не менее двух букв");
+        isRight = false;
+    }
+    if (order.details.address.city < 2) {
+        createNote("длина названия города должна быть не менее двух букв");
+        sRight = false;
+    }
+    if (order.details.address.street < 2) {
+        createNote("длина названия улицы должна быть не менее двух букв");
+        sRight = false;
+    }
+    if (order.details.address.house < 1) {
+        createNote("длина номера дома должна быть не менее двух букв");
+        sRight = false;
+    }
+    if (order.details.address.apartment < 1) {
+        createNote("длина номера квартиры должна быть не менее двух букв");
+        sRight = false;
+    }
+    return isRight;
+}
+
+
+function AddProduct(index){
+    this.id = index+1;
+    this.size = `${allProducts[index].size}`,
+    this.crust = "cheesy"
+}
+
+buttonSubmit.addEventListener("click", function(){
+    let order = {
+        pizzas:[],
+        details: {
+            user: {
+                firstname: document.querySelector("[name=firstName]").value,
+                lastname: document.querySelector("[name=lastName]").value,
+                birthdate: document.querySelector("[name=dateOfBirth]").value,
+            },
+            address:{
+                city: document.querySelector("[name=city]").value,
+                street: document.querySelector("[name=street]").value,
+                house: document.querySelector("[name=house]").value,
+                apartment: document.querySelector("[name=flat]").value,
+                comment: document.querySelector("[name=comment]").value,
+            }
+        }
+    }
+
+    order.details.user.registrationAddress = JSON.stringify(order.details.address);
+
+    // если человек заказл несколько продуктов одного типа, то они просто добавятся последовательно
+    for (const element of boughtProducts) {
+        for (let i = 0; i < element.count; i++) {
+            order.pizzas.push(new AddProduct(+element.index));
+        }
+    }
+
+    if(check(order)){
+        // если всё заполнено правильно, то отправляется запрос(сейчас выдаёт Failed to load resource: the server responded with a status of 405 () и выключается страничка)
+        let response = fetch('api/pizza/createOrder', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(order)
+        });
+    }
+    
 })
--- js/script.js
+++ js/script.js
@@ -17,29 +17,26 @@ function CreateProduct(name, size, structure, cost, type, image){
 
 // Добавление продуктов
 const allProducts = [
-    new CreateProduct("Карбонара", "30см", "Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок", 749, "recomendated stock", "img/pizzas/Carbonara.png"),
+    new CreateProduct("Карбонара", "30см", "Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок", 749, "recomendated stock", "assets/img/pizzas/Carbonara.png"),
 
-    new CreateProduct("Аррива!", "30см", "Цыпленок, острая чоризо, соус бургер, сладкий перец, красный лук, томаты, моцарелла", 649, "recomendated sweet", "img/pizzas/Arriva.png"),
+    new CreateProduct("Аррива!", "30см", "Цыпленок, острая чоризо, соус бургер, сладкий перец, красный лук, томаты, моцарелла", 649, "recomendated sweet", "assets/img/pizzas/Arriva.png"),
 
-    new CreateProduct("Диабло", "30см", "Острая чоризо, острый перец халапеньо, соус барбекю, митболы, томаты", 749, "recomendated", "img/pizzas/Diablo.png"),
+    new CreateProduct("Диабло", "30см", "Острая чоризо, острый перец халапеньо, соус барбекю, митболы, томаты", 749, "recomendated", "assets/img/pizzas/Diablo.png"),
 
-    new CreateProduct("Овощи и грибы", "30см", "Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус", 549, "recomendated without-meat", "img/pizzas/VegMush.png"),
+    new CreateProduct("Овощи и грибы", "30см", "Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус", 549, "recomendated without-meat", "assets/img/pizzas/VegMush.png"),
 
-    new CreateProduct("Карбонара", "20см", "Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок", 649, "recomendated stock", "img/pizzas/Carbonara.png"),
+    new CreateProduct("Карбонара", "20см", "Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок", 649, "recomendated stock", "assets/img/pizzas/Carbonara.png"),
 
-    new CreateProduct("Аррива!", "20см", "Цыпленок, острая чоризо, соус бургер, сладкий перец, красный лук, томаты, моцарелла", 549, "recomendated sweet", "img/pizzas/Arriva.png"),
+    new CreateProduct("Аррива!", "20см", "Цыпленок, острая чоризо, соус бургер, сладкий перец, красный лук, томаты, моцарелла", 549, "recomendated sweet", "assets/img/pizzas/Arriva.png"),
 
-    new CreateProduct("Диабло", "20см", "Острая чоризо, острый перец халапеньо, соус барбекю, митболы, томаты", 649, "recomendated", "img/pizzas/Diablo.png"),
+    new CreateProduct("Диабло", "20см", "Острая чоризо, острый перец халапеньо, соус барбекю, митболы, томаты", 649, "recomendated", "assets/img/pizzas/Diablo.png"),
 
-    new CreateProduct("Овощи и грибы", "20см", "Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус", 449, "recomendated without-meat", "img/pizzas/VegMush.png")
+    new CreateProduct("Овощи и грибы", "20см", "Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус", 449, "recomendated without-meat", "assets/img/pizzas/VegMush.png")
 ]
 
-localStorage.setItem("countOfProducts", allProducts.length);
-for (const index in allProducts) {
-    localStorage.setItem(index, JSON.stringify(allProducts[index]));
-}
+const blockForProducts = document.querySelector(".products");
 
-const blockForProducts = document.querySelector('.products');
+localStorage.setItem("allProducts", JSON.stringify(allProducts));
 
 for (const product of allProducts) {
     blockForProducts.insertAdjacentHTML(
@@ -56,8 +53,8 @@ for (const product of allProducts) {
             <p class="item__text">${product.structure}</p>
             <span class="item__cost">${product.cost} ₽</span>
             <button class="item__purchase">
-                <img src="img/bagBlack.png" alt="">
-                <img class="hidden" src="img/bagWhite.png" alt="">
+                <img src="assets/img/bagBlack.png" alt="">
+                <img class="hidden" src="assets/img/bagWhite.png" alt="">
             </button>
         </div>
         `
@@ -112,13 +109,13 @@ blockForProducts.addEventListener("click", function(event){
         index = findIndexOfPizza(blockForProducts.children, buttonNow.parentElement);
 
         if(buttonNow.classList.contains("bought")){
-            boughtProducts.push(index);
+            boughtProducts.push({index: `${index}`, count: 1});
         }
         else{
-            boughtProducts.splice(boughtProducts.indexOf(index), 1);
+            boughtProducts.splice(boughtProducts.find(item => item.index==index), 1);
         }
 
-        localStorage.setItem("boughtProducts", boughtProducts.join(","));
+        localStorage.setItem("boughtProducts", JSON.stringify(boughtProducts));
         localStorage.setItem("count", boughtProducts.length);
         orders.textContent = `Оформить заказ(${boughtProducts.length})`;
     }
